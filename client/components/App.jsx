26e03845262e3d6e9c9fe215f2c5c938fad0d02b
Matthew Egan <69609987+mattegan111@users.noreply.github.com>
2022-09-19 19:09:38 +1200

    Add map on page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { fetchFruits } from '../actions'
+import mapboxgl from '!mapbox-gl'
+import {API_KEY} from '../../secrets'
+
+mapboxgl.accessToken = API_KEY
 
 function App() {
   const fruits = useSelector((state) => state.fruits)
@@ -10,9 +14,25 @@ function App() {
     dispatch(fetchFruits())
   }, [])
 
+  const mapContainer = useRef(null)
+  const map = useRef(null)
+  const [lng, setLng] = useState(-70.9)
+  const [lat, setLat] = useState(42.35)
+  const [zoom, setZoom] = useState(9)
+
+  useEffect(() => {
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    })
+  }, !map.current)
+
   return (
     <>
       <div className="app">
+        <div ref={mapContainer} className="map-container" />
         <h1>Fullstack Boilerplate - with Fruits!</h1>
         <ul>
           {fruits.map((fruit) => (
